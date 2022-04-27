e36216e86d727878f0c5aa03e4ef7d62b854d6fe
Michele Bisceglia <mikael_bisceglia@libero.it>
2022-04-27 16:13:44 +0200

    commenti traccia

---
+++ js/script.js
@@ -0,0 +1,20 @@
+/*Descrizione:
+Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+Consigli del giorno:
+* Pensate prima in italiano.
+* Dividete in piccoli problemi la consegna.
+* Individuate gli elementi di cui avete bisogno per realizzare il programma.*/
+
+//Creare un array
+
+//Inserire nell'array 5 numeri casuali
+
+//Stampare i 5 numeri nella pagina per 30 secondi
+
+//chiedere all'utente di inserire uno alla volta 5 numeri presenti nell'array(5 promt)
+
+//controllare se questi 5 numeri sono presenti nell'arrey
+
+//stampare nella pagina i numeri scritti dall'utente presenti anche nell'array e il punteggio(numero di numeri che corrispondono)
