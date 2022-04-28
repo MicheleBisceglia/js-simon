9c6f42d94c2775b64c7f34e6b902eba41ac492a2
Michele Bisceglia <mikael_bisceglia@libero.it>
2022-04-28 11:30:49 +0200

    add time function

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
-/*Descrizione:
+/*
 Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
 Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
-Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
-Consigli del giorno:
-* Pensate prima in italiano.
-* Dividete in piccoli problemi la consegna.
-* Individuate gli elementi di cui avete bisogno per realizzare il programma.*/
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.*/
 
-function generateRndNumbers(maxLimit) {
+
+    //stampare i 5 numeri nell'html
+const generateArray = generateRndNumbers (5 ,100);
+document.querySelector(".numbers").innerHTML = generateArray; 
+    console.log(generateArray);
+
+function generateRndNumbers(min,max) {
     //Crea un array
     const numbersArray = [];
     //creare 5 numeri casuali
     while (numbersArray.length < 5) {
-        const number = rndNumber (1, maxLimit)
+        const number = rndNumber (1, min,max)
     //inserire i 5 numeri nell'array
         if ( !numbersArray.includes(number)) {
             numbersArray.push(number);
@@ -25,17 +27,11 @@ function generateRndNumbers(maxLimit) {
         return Math.floor(Math.random() * (max - min +1) ) + min;    
     }    
 }
-    //stampare i 5 numeri nell'html
-const generateArray = generateRndNumbers (5 ,100);
-document.querySelector(".numbers").innerHTML = generateArray; 
-    console.log(generateArray);
 
-    
+setTimeout(function() {
+    document.querySelector(".numbers").innerHTML = "";
+}, 3000)
 
-
-//Inserire nell'array 5 numeri casuali
-
-//Stampare i 5 numeri nella pagina per 30 secondi
 
 //chiedere all'utente di inserire uno alla volta 5 numeri presenti nell'array(5 promt)
 
